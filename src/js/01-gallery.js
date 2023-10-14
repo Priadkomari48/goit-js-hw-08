5e4c629250db25449df05db25e7e4c1bbbc91759
Priadkomari48 <128245032+Priadkomari48@users.noreply.github.com>
2023-10-14 10:14:19 +0100

    ubdate

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+ return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+    `;
+}).join('');
+
+listEl.innerHTML = markup;
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  close: true,
+  enableKeyboard: true,
+})
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,62 @@
+import { throttle } from 'lodash';
+
+// Пошук елементів на сторінці
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state'; // Ключ для сховища
+
+// Додаємо слухача події input до форми
+form.addEventListener(
+  'input',
+  throttle(e => {
+
+  
+    const objectToSave = { email: email.value, message: message.value };
+
+   
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectToSave));
+  }, 500) 
+);
+
+
+form.addEventListener('submit', e => {
+
+  e.preventDefault(); 
+
+  
+  if (email.value === '' || message.value === '') {
+    return alert('Заповніть всі поля!');
+  }
+
+
+  console.log({ email: email.value, message: message.value });
+
+  form.reset(); 
+  localStorage.removeItem(LOCALSTORAGE_KEY); 
+});
+
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+
+  
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+
+   
+    console.error('Get state error: ', error.message);
+  }
+};
+
+
+const storageData = load(LOCALSTORAGE_KEY);
+
+// Перевірка стану сховища.
+// Якщо  в сховищі є збережені дані - заповнити ними поля форми.
+if (storageData) {
+  email.value = storageData.email;
+  message.value = storageData.message;
+} // В іншому випадку поля будуть порожніми
 
